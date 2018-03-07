46e2081a0d13fb1d446ab622e211c1bd8f2f78d2
qixin <qixin@haoqix.com>
2018-03-07 21:11:11 +0800

    project add to gitHub

---
+++ DemoApp/DemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoApp
+//
+//  Created by qixin on 2018/3/7.
+//  Copyright © 2018年 qixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
