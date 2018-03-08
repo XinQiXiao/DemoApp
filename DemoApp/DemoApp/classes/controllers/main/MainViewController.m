c7be829138e76305e08a1b4d04b8c60c6216e1c1
qixin <qixin@haoqix.com>
2018-03-08 18:58:13 +0800

    import masonry lib

--- DemoApp/DemoApp/classes/controllers/main/MainViewController.m
+++ DemoApp/DemoApp/classes/controllers/main/MainViewController.m
@@ -9,6 +9,7 @@
 #import "MainViewController.h"
 #import "MediaViewController.h"
 #import "PhotoViewController.h"
+#import "TestViewController.h"
 #import "Contants.h"
 
 @interface MainViewController ()
@@ -29,10 +30,22 @@
     [mediaBtn setTitleColor:[UIColor blueColor] forState:(UIControlStateNormal)];
     [mediaBtn addTarget:self action:@selector(toMedia) forControlEvents:(UIControlEventTouchUpInside)];
     [self.view addSubview:mediaBtn];
+    
+    CGFloat testOriginY = mediaOriginY+mediaHeight+20.0;
+    CGFloat testHeight = 30.0;
+    UIButton *testBtn = [[UIButton alloc]initWithFrame:CGRectMake(100, testOriginY, 100, testHeight)];
+    [testBtn setTitle:@"测试" forState:(UIControlStateNormal)];
+    [testBtn setTitleColor:[UIColor blueColor] forState:(UIControlStateNormal)];
+    [testBtn addTarget:self action:@selector(toTest) forControlEvents:(UIControlEventTouchUpInside)];
+    [self.view addSubview:testBtn];
 }
 
 -(void)toMedia{
     [self.navigationController pushViewController:[PhotoViewController new] animated:YES];
+}
+
+-(void)toTest{
+    [self.navigationController pushViewController:[TestViewController new] animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
--- DemoApp/DemoApp/classes/controllers/test/TestViewController.m
+++ DemoApp/DemoApp/classes/controllers/test/TestViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TestViewController.h"
+#import <Masonry.h>
 
 @interface TestViewController ()
 
@@ -16,22 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = @"Test";
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
