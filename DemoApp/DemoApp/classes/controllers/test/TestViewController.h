7f528113db129874eaed34d4c5195838598966f2
qixin <qixin@haoqix.com>
2018-03-08 12:25:25 +0800

    test controller add

---
+++ DemoApp/DemoApp/classes/controllers/test/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  DemoApp
+//
+//  Created by qixin on 2018/3/8.
+//  Copyright © 2018年 qixin. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface TestViewController : BaseViewController
+
+@end
---
+++ DemoApp/DemoApp/classes/controllers/test/TestViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestViewController.m
+//  DemoApp
+//
+//  Created by qixin on 2018/3/8.
+//  Copyright © 2018年 qixin. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
